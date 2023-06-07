ed8926ae8572f8f1397dfcecaa0c52c38b90e642
VictorienDruon <victoriendruon@icloud.com>
2023-06-07 20:09:35 +0200

    UPDATE siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
 	name: "Hobby Hunter",
-	description: "Share your tastes to the world. With style.",
+	description: "Share your tastes to the world",
 	links: {
 		twitter: "https://twitter.com/druon_victorien",
 		github: "https://github.com/VictorienDruon/Hobby-Hunter",
