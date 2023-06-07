e39f99013786da61e76914abe6d8679676cf0322
VictorienDruon <victoriendruon@icloud.com>
2023-06-07 20:20:11 +0200

    fix: responsive border avatar home

--- src/features/home/components/user-coverflow/components/Avatar.tsx
+++ src/features/home/components/user-coverflow/components/Avatar.tsx
@@ -27,7 +27,7 @@ const getInitials = (name: string) => {
 
 const Avatar = ({ name, image }: AvatarProps) => {
 	return (
-		<AvatarWrapper className="z-10 h-20 w-20 border-4 border-background bg-background md:h-28 md:w-28 lg:h-36 lg:w-36">
+		<AvatarWrapper className="z-10 h-20 w-20 border-2 border-background bg-background md:h-28 md:w-28 lg:h-36 lg:w-36 lg:border-4">
 			<AvatarImage className="object-cover" src={image ?? undefined} />
 			<AvatarFallback className="select-none text-2xl tracking-widest md:text-3xl lg:text-4xl">
 				{getInitials(name)}
